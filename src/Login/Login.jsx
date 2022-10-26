de6b63a2a85f6dccde7a8f7ed11851eb82f701ad
Shivansh-soni <shivanshsoni.indore@gmail.com>
2022-10-26 13:34:41 +0530

    teams and mores

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -84,48 +84,50 @@ function Hero() {
             </div>
             <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
               <div className="card-body ml-3">
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Email</span>
-                  </label>
-                  <input
-                    type="text"
-                    value={username}
-                    onChange={(e) => {
-                      setUsername(e.target.value);
-                    }}
-                    placeholder="Email"
-                    className="input input-bordered"
-                  />
-                </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Password</span>
-                  </label>
-                  <input
-                    value={password}
-                    onChange={(e) => {
-                      setPassword(e.target.value);
-                    }}
-                    type="password"
-                    placeholder="Password"
-                    className="input input-bordered"
-                  />
-                  <label className="label">
-                    <a href="/#" className="label-text-alt link link-hover">
-                      Forgot password?
-                    </a>
-                  </label>
-                </div>
-                <div className="form-control mt-6">
-                  <button
-                    type="submit"
-                    onClick={handleSubmit}
-                    className="btn btn-primary"
-                  >
-                    Login
-                  </button>
-                </div>
+                <form action="">
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">Email</span>
+                    </label>
+                    <input
+                      type="text"
+                      value={username}
+                      onChange={(e) => {
+                        setUsername(e.target.value);
+                      }}
+                      placeholder="Email"
+                      className="input input-bordered"
+                    />
+                  </div>
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">Password</span>
+                    </label>
+                    <input
+                      value={password}
+                      onChange={(e) => {
+                        setPassword(e.target.value);
+                      }}
+                      type="password"
+                      placeholder="Password"
+                      className="input input-bordered"
+                    />
+                    <label className="label">
+                      <a href="/#" className="label-text-alt link link-hover">
+                        Forgot password?
+                      </a>
+                    </label>
+                  </div>
+                  <div className="form-control mt-6">
+                    <button
+                      type="submit"
+                      onClick={handleSubmit}
+                      className="btn btn-primary"
+                    >
+                      Login
+                    </button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
